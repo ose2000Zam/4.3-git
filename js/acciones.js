2cffea788a7ee1b830ce98b73b16aa3cc8af47db
ose2000Zam <jose.escalona.cbtis16@gmail.com>
2017-04-26 00:15:15 -0500

    2subida
    cuarta aplicacion

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@ $(document).ready(function (e){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
 		$('#disp table td').eq(7).text(device.platform);
-		$('#disp table td').eq(9).text(device.vesion);
+		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
 		escribehistoria ('la app se pauso');
